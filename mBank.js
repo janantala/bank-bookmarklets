483975ae3a15d5057a089b57968f1e2213654fbe
Jan Antala <jan@antala.sk>
2014-02-26 15:29:18 +0100

    style: convert indentation to spaces

--- mBank.js
+++ mBank.js
@@ -1,19 +1,20 @@
 javascript:(
 
-	/*************** mBank LOGIN ***************/
-	
-	function(){
-		var LOGIN_ID = "00000000" // your login id
-		var PASSWORD = "password" // your password
-		var URL = "https://online.mbank.sk/sk/Login" // bank login url
+  /*************** mBank LOGIN ***************/
 
-		if (document.location.href !== URL){
-			document.location.href = URL;
-		}
-		else {
-			document.getElementById("userID").value = LOGIN_ID;
-			document.getElementById("pass").value = PASSWORD;
-			document.getElementById("pass").click();
-		}
-	}
+  function(){
+    var LOGIN_ID = "00000000" // your login id
+    var PASSWORD = "password" // your password
+    var URL = "https://online.mbank.sk/sk/Login" // bank login url
+
+    if (document.location.href !== URL){
+      document.location.href = URL;
+    }
+    else {
+      document.getElementById("userID").value = LOGIN_ID;
+      document.getElementById("userID").focus();
+      document.getElementById("pass").value = PASSWORD;
+      document.getElementById("pass").focus();
+    }
+  }
 )();
