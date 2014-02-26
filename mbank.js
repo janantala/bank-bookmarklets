f70ffed5338d82a08a50629255450e9e3e60ba33
Jan Antala <jan@antala.sk>
2014-02-26 15:04:56 +0100

    feat(mbank): create mbank bookmarklet

---
+++ mbank.js
@@ -0,0 +1,19 @@
+javascript:(
+
+	/*************** mBank LOGIN ***************/
+	
+	function(){
+		var LOGIN_ID = "00000000" // your login id
+		var PASSWORD = "password" // your password
+		var URL = "https://online.mbank.sk/sk/Login" // bank login url
+
+		if (document.location.href !== URL){
+			document.location.href = URL;
+		}
+		else {
+			document.getElementById("userID").value = LOGIN_ID;
+			document.getElementById("pass").value = PASSWORD;
+			document.getElementById("pass").click();
+		}
+	}
+)();
