399d0437544531c75d3e9e1444bb1b19fd3eb06e
Jan Antala <jan@antala.sk>
2014-02-26 15:29:43 +0100

    style: convert indentation to spaces

--- mBank old.js
+++ mBank old.js
@@ -1,15 +1,15 @@
 javascript:(
 
-	/*************** mBank LOGIN ***************/
+  /*************** mBank LOGIN ***************/
 
-	function(){
-		var LOGIN_ID = "00000000" // your login id
-		var PASSWORD = "password" // your password
-		var URL = "https://sk.mbank.eu/" // bank login url
+  function(){
+    var LOGIN_ID = "00000000" // your login id
+    var PASSWORD = "password" // your password
+    var URL = "https://sk.mbank.eu/" // bank url
 
-		document.location.href = URL;
-		document.getElementById("customer").value = LOGIN_ID;
-		document.getElementById("password").value = PASSWORD;
-		document.getElementById("confirm").click();
-	}
+    document.location.href = URL;
+    document.getElementById("customer").value = LOGIN_ID;
+    document.getElementById("password").value = PASSWORD;
+    document.getElementById("confirm").click();
+  }
 )();
