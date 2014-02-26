a4f57a68237019ec9ec0349abdb60a8f8c09d714
Jan Antala <jan@antala.sk>
2014-02-26 15:32:37 +0100

    style: convert indentation to spaces

--- UniCredit Bank.js
+++ UniCredit Bank.js
@@ -1,22 +1,22 @@
 javascript:(
-	
-	/*************** UniCredit Bank LOGIN ***************/
+  
+  /*************** UniCredit Bank LOGIN ***************/
 
-	function(){
-		var LOGIN_ID = "00000000" // your login id
-		var PASSWORD = "1234567890" // your password
-		var URL = "https://sk.unicreditbanking.net/disp?link=login.tplogin.system_login" // bank login url
+  function(){
+    var LOGIN_ID = "00000000" // your login id
+    var PASSWORD = "1234567890" // your password
+    var URL = "https://sk.unicreditbanking.net/disp?link=login.tplogin.system_login" // bank login url
 
-		if (document.location.href !== URL){
-			document.location.href = URL;
-		}
-		else {
-			var id = document.getElementsByTagName('input')[8];
-			id.value = LOGIN_ID;
-			id.focus();
-			var pass = document.getElementsByTagName('input')[9];
-			pass.value =PASSWORD;
-			pass.focus();
-		}
-	}
+    if (document.location.href !== URL){
+      document.location.href = URL;
+    }
+    else {
+      var id = document.getElementsByTagName('input')[8];
+      id.value = LOGIN_ID;
+      id.focus();
+      var pass = document.getElementsByTagName('input')[9];
+      pass.value =PASSWORD;
+      pass.focus();
+    }
+  }
 )();
